f758491b7e448b53eb912b0b94440261da0de41e
Thomas Bolon <thomas.bolon@gmail.com>
2010-03-18 18:03:05 +0100

    NUnit test project added

---
+++ Tests/Pertinent/Text/ParserTests.cs
@@ -0,0 +1,145 @@
+﻿using NUnit.Framework;
+
+namespace Pertinent.Text
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        [Test]
+        public void Empty()
+        {
+            TextTemplate t = new TextTemplate("");
+            Assert.AreEqual(0, t.Parameters.Count);
+        }
+
+        [Test]
+        public void Only_Content()
+        {
+            TextTemplate t = new TextTemplate("foo bar");
+            Assert.AreEqual(0, t.Parameters.Count);
+        }
+
+        [Test]
+        public void Only_Parameter()
+        {
+            TextTemplate t = new TextTemplate("[foo]");
+            Assert.AreEqual(1, t.Parameters.Count);
+        }
+
+        [Test]
+        public void One()
+        {
+            TextTemplate t = new TextTemplate("ab [abd]");
+            Assert.AreEqual(1, t.Parameters.Count);
+        }
+
+        [Test]
+        public void MultiLine()
+        {
+            TextTemplate t = new TextTemplate("[fo\r\no]");
+            Assert.AreEqual("fo\r\no", t.Parameters[0].Name);
+        }
+
+        [Test]
+        public void Detect_Name()
+        {
+            TextTemplate t = new TextTemplate("ab [abd]");
+            Assert.AreEqual("abd", t.Parameters[0].Name);
+        }
+
+        [Test]
+        public void Ingore_Empty()
+        {
+            TextTemplate t = new TextTemplate("ab[]");
+            Assert.AreEqual(0, t.Parameters.Count);
+        }
+
+        [Test]
+        public void Two()
+        {
+            TextTemplate t = new TextTemplate("[foo] [bar]");
+            Assert.AreEqual(2, t.Parameters.Count);
+        }
+
+        [Test]
+        public void Correct_Index()
+        {
+            TextTemplate t = new TextTemplate("[foo] [bar]");
+            Assert.AreEqual(6, t.Parameters[1].Index);
+        }
+
+        [Test]
+        public void Correct_Length()
+        {
+            TextTemplate t = new TextTemplate("[foo] [barr]");
+            Assert.AreEqual(6, t.Parameters[1].Length);
+        }
+
+        [Test]
+        public void Detect_DefaultValue()
+        {
+            TextTemplate t = new TextTemplate("[foo:vide]");
+            Assert.AreEqual("vide", t.Parameters[0].DefaultValue);
+            Assert.AreEqual(null, t.Parameters[0].Format);
+        }
+
+        [Test]
+        public void Detect_Empty_DefaultValue()
+        {
+            TextTemplate t = new TextTemplate("[foo:]");
+            Assert.AreEqual("", t.Parameters[0].DefaultValue);
+            Assert.AreEqual(null, t.Parameters[0].Format);
+        }
+
+        [Test]
+        public void Detect_Empty_DefaultValue_And_Empty_Format()
+        {
+            TextTemplate t = new TextTemplate("[foo::]");
+            Assert.AreEqual("", t.Parameters[0].DefaultValue);
+            Assert.AreEqual("", t.Parameters[0].Format);
+        }
+
+        [Test]
+        public void Detect_Format()
+        {
+            TextTemplate t = new TextTemplate("[foo::bar]");
+            Assert.AreEqual("", t.Parameters[0].DefaultValue);
+            Assert.AreEqual("bar", t.Parameters[0].Format);
+        }
+
+        [Test]
+        public void Allow_Escaped_Name()
+        {
+            TextTemplate t = new TextTemplate(@"[fo\o]");
+            Assert.AreEqual("foo", t.Parameters[0].Name);
+        }
+
+        [Test]
+        public void Allow_Escaped_DoubleDot_In_Name()
+        {
+            TextTemplate t = new TextTemplate(@"[fo\:o]");
+            Assert.AreEqual("fo:o", t.Parameters[0].Name);
+        }
+
+        [Test]
+        public void Allow_Escaped_DefaultValue()
+        {
+            TextTemplate t = new TextTemplate(@"[foo:b\:oo]");
+            Assert.AreEqual("b:oo", t.Parameters[0].DefaultValue);
+        }
+
+        [Test]
+        public void Allow_Escaped_Ending_Squares()
+        {
+            TextTemplate t = new TextTemplate(@"[foo:b\]oo]");
+            Assert.AreEqual("b]oo", t.Parameters[0].DefaultValue);
+        }
+
+        [Test]
+        public void Non_Ending_Parameter_Are_Ignored()
+        {
+            TextTemplate t = new TextTemplate(@"[foo] [boo");
+            Assert.AreEqual(1, t.Parameters.Count);
+        }
+    }
+}
---
+++ Tests/Pertinent/Text/TemplateTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Pertinent.Text
+{
+    [TestFixture]
+    public class TemplateTests
+    {
+        [Test]
+        public void Empty()
+        {
+            var t = new TextTemplate("");
+            Assert.AreEqual("", t.Execute());
+        }
+
+        [Test]
+        public void Single()
+        {
+            var t = new TextTemplate("a");
+            Assert.AreEqual("a", t.Execute());
+        }
+
+        [Test]
+        public void Multi()
+        {
+            var t = new TextTemplate("foo bar");
+            Assert.AreEqual("foo bar", t.Execute());
+        }
+
+        [Test]
+        public void Single_Parameter()
+        {
+            var t = new TextTemplate("[foo]");
+            Assert.AreEqual("", t.Execute());
+        }
+
+        [Test]
+        public void Single_Parameter_With_Default_Value()
+        {
+            var t = new TextTemplate("[foo:bar]");
+            Assert.AreEqual("bar", t.Execute());
+        }
+
+        [Test]
+        public void Single_Parameter_With_Format()
+        {
+            var t = new TextTemplate("[foo:bar:Hello {0}!]");
+            Assert.AreEqual("Hello bar!", t.Execute());
+        }
+
+        [Test]
+        public void Single_Parameter_Empty_With_Format()
+        {
+            var t = new TextTemplate("[foo::Hello {0}!]");
+            Assert.AreEqual("Hello bar!", t.Execute());
+        }
+    }
+}
